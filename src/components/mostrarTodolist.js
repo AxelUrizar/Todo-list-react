69f44d0ab7ef4b273afcc4dc510106ed5047bd66
FakeFly <axelurizarmartin@gmail.com>
2022-02-15 15:42:50 +0000

    todo-list inicio

---
+++ src/components/mostrarTodolist.js
@@ -0,0 +1,9 @@
+import db from '../db'
+
+function MostrarTodolist () {
+    return (
+        db
+    )
+}
+
+export default  MostrarTodolist
---
+++ src/db.js
@@ -0,0 +1,21 @@
+const db =
+    [
+        {
+            id: 1,
+            title: 'Holaquetal',
+            isComplete: false
+        },
+        {
+            id: 2,
+            title: 'Holaqueta2l',
+            isComplete: false
+        },
+        {
+            id: 3,
+            title: 'Holaquetal3',
+            isComplete: true
+        }
+    ]
+
+export default db
+
